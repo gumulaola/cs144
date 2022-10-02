fca1edf16f8a4acda36da651181aecd744fc4e65
gumulaola <fx96@stu.pku.edu.cn>
2022-10-02 14:26:33 +0000

    rename some vals in lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -29,12 +29,12 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
         _isn = header.seqno;
         _set_syn_flag = true;
     }
-    uint64_t abs_ackno = _reassembler.stream_out().bytes_written() + 1;
-    uint64_t curr_abs_seqno = unwrap(header.seqno, _isn, abs_ackno);
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written() + 1;
+    uint64_t absolute_seqno = unwrap(header.seqno, _isn, checkpoint);
 
     //! NOTE: SYN 包中的 payload 不能被丢弃
     //! NOTE: reassember 足够鲁棒以至于无需进行任何 seqno 过滤操作
-    uint64_t stream_index = curr_abs_seqno - 1 + (header.syn);
+    uint64_t stream_index = absolute_seqno - 1 + (header.syn);
     _reassembler.push_substring(seg.payload().copy(), stream_index, header.fin);
 }
 
@@ -47,7 +47,7 @@ optional<WrappingInt32> TCPReceiver::ackno() const {
     // 如果当前处于 FIN_RECV 状态，则还需要加上 FIN 标志长度
     if (_reassembler.stream_out().input_ended())
         ++abs_ack_no;
-    return WrappingInt32(_isn) + abs_ack_no;
+    return wrap(abs_ack_no, _isn);
 }
 
 size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
